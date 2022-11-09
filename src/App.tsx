9962f856dbb4d81289e26c11226f73988b197e32
egoryan8 <fogel_92@bk.ru>
2022-11-09 14:41:30 +0700

    feat: combineReducer, Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import UserList from "./components/UserList";
 
 function App() {
   return (
     <div>
       Приложение на React Redux TypeScript
+        <UserList/>
     </div>
   );
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import {applyMiddleware, createStore} from "redux";
 import thunk from "redux-thunk";
+import {rootReducer} from "./reducers";
 
-export const store = createStore({}, applyMiddleware(thunk));
+export const store = createStore(rootReducer, applyMiddleware(thunk));
